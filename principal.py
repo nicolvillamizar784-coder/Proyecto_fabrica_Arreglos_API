cfb6dbc18eb3f7d8a927ea2ba0eb8b14ab2f4df4
nicolvillamizar784-coder <nicolvillamizar784@gmail.com>
2026-02-09 08:27:51 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,12 +1,12 @@
 from Base_datos_empleados import Base_datos_empleados
 from Empleado_modelo import Empleado_modelo
-
+from diccionario import datos_diccionario
 
 #creo la base de datos de empleados
 obj_db_empleado_lista = Base_datos_empleados()
 #creo el objeto que voy a guardar
-obj_empleado1 = Empleado_modelo("Juan", "Perez", "123456789")
-obj_empleado2 = Empleado_modelo("Ana", "Gomez", "987654321")
+obj_empleado1 = Empleado_modelo("sebastian", "contreras", "123455689")
+obj_empleado2 = Empleado_modelo("natalia", "quintero", "9879894321")
 
 
 #creo una lista para guardar masivamente
@@ -19,4 +19,15 @@ obj_db_empleado_lista.guardar_empleado(obj_empleado2)
 obj_db_empleado_lista.extender_varios_empleados(lista_nuevos_empleados)
 
 #limpiar toda la lista de empleados
+
 obj_db_empleado_lista.imprimir_info()
+obj_diccionario = datos_diccionario()
+info = obj_diccionario.sacar_valores()
+print(info)
+
+
+nuevo_diccionario = {"1093687321": {"nombre":"marcos", "apellido": "rosales", "maquina": ("maquina pinturas",
+"maquina hidraulica")}}
+
+obj_diccionario.acttualizar_info(nuevo_diccionario)
+obj_diccionario.imprimir_info()
